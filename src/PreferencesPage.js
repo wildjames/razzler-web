646cb6501baaa611d63cef2b8c49457cf09a5f1e
Jim Wild <james.wild@airelogic.com>
2024-06-12 17:58:24 +0100

    Add reset all button to the page

--- src/PreferencesPage.js
+++ src/PreferencesPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Typography, TextField, Button } from "@mui/material";
+import { Container, Typography, TextField, Button, Grid } from "@mui/material";
 import { useSnackbar } from "notistack";
 
 function PreferencesPage() {
@@ -7,38 +7,38 @@ function PreferencesPage() {
   const [loading, setLoading] = useState(true);
   const { enqueueSnackbar } = useSnackbar();
 
-  useEffect(() => {
-    const fetchPreferences = async () => {
-      const token = localStorage.getItem("razzler_login_token");
-      if (!token) {
-        console.log("No token found, user is not logged in");
-        window.location.href = "/";
-      }
-      try {
-        const response = await fetch(
-          `${process.env.REACT_APP_API_BASE_URL}/preferences`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch preferences");
+  const fetchPreferences = async () => {
+    const token = localStorage.getItem("razzler_login_token");
+    if (!token) {
+      console.log("No token found, user is not logged in");
+      window.location.href = "/";
+    }
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_BASE_URL}/preferences`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
         }
-        const data = await response.json();
-
-        // The response includes the user_id, which we don't need. Remove it from the preferences object
-        delete data.user_id;
-
-        setPreferences(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching preferences:", error);
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch preferences");
       }
-    };
+      const data = await response.json();
 
+      // The response includes the user_id, which we don't need. Remove it from the preferences object
+      delete data.user_id;
+
+      setPreferences(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching preferences:", error);
+    }
+  };
+
+  useEffect(() => {
     fetchPreferences();
   }, []);
 
@@ -78,6 +78,36 @@ function PreferencesPage() {
     }
   };
 
+  const handleReset = async () => {
+    const token = localStorage.getItem("razzler_login_token");
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_BASE_URL}/preferences`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log("Preferences Reset Response: ", data);
+      if (!response.ok) {
+        console.error("Failed to reset preferences:", response);
+        throw new Error("Failed to reset preferences");
+      }
+      enqueueSnackbar("Preferences reset successfully!", {
+        variant: "success",
+      });
+    } catch (error) {
+      console.error("Error resetting preferences:", error);
+      enqueueSnackbar("Failed to reset preferences", { variant: "error" });
+    }
+
+    fetchPreferences();
+  };
+
   return (
     <Container component="main" maxWidth="md">
       <Typography component="h1" variant="h5">
@@ -98,14 +128,18 @@ function PreferencesPage() {
           />
         ))
       )}
-      <Button
-        onClick={handleSubmit}
-        variant="contained"
-        color="primary"
-        style={{ marginTop: 20 }}
-      >
-        Save Changes
-      </Button>
+      <Grid container spacing={2} justifyContent="space-between">
+        <Grid item>
+          <Button onClick={handleSubmit} variant="contained" color="primary">
+            Save Changes
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={handleReset} variant="contained" color="secondary">
+            Reset to Default
+          </Button>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
